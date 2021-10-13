704293b29cbd2d3dfba8f5d335fb950eaee33044
Greg Behnke <gregbehnke24x7@msn.com>
2021-10-13 09:47:34 -0500

    commit progress

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,45 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// view the data
+console.log(tableData);
+
+
+// reference variables
+var $tbody = d3.select("tbody");
+var button = d3.select("#filter-btn");
+var inputFieldDate = d3.select("#datetime");
+var inputFieldCity = d3.select("#city");
+var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"]
+
+// add the data to the page
+var addData = (dataInput) => {
+    dataInput.forEach(ufoSightings => {
+        var row = $tbody.append("tr");
+        columns.forEach(column => row.append("td").text(ufoSightings[column])
+        )
+    });
+}
+
+addData(tableData);
+
+
+// create event listener for button clickiness
+button.on("click", () => {
+
+    d3.event.preventDefault();
+    var inputDate = inputFieldDate.property("value").trim();
+    var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
+    $tbody.html("");
+
+    let response = {
+        filterDate
+    }
+
+    if(response.filterDate.length !== 0) {
+        addData(filterDate);
+    }
+        else {
+            $tbody.append("tr").append("td").text("No Sightings Here...Move On...");
+        }
+})
